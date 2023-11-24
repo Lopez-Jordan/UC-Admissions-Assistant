2d63ca48b41b2d47f3bac972c6e481a191feb1e7
Jordan Lopez <jordanlopezemail@gmail.com>
2023-11-24 14:37:23 -0800

    homepage is coming along

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
+import './App.css';
+
 export default function App() {
   
   return (
     <>
-      <h1>hello world!</h1>
+      <section className="page1">
+        <div className="half1">
+          <div className="content1">
+            <h2 id='message'>message a ...</h2>
+            <h1 id='mainHeading'>UC Admission Officer</h1>
+          </div>
+          <img id="emblem" src="/emblem.png" alt="" />
+        </div>
+
+
+        <div className="half2">
+          <div className='content2'>
+          
+          </div>
+        </div>
+
+      </section>
     </>
   )
 }
